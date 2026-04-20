b322515efff6729670f64753277fc285dac58564
Roshni <roshnimuruganathan@gmail.com>
2026-04-20 19:09:58 +0530

    Add files via upload

--- CustomCell/ViewController.swift
+++ CustomCell/ViewController.swift
@@ -43,7 +43,7 @@ class ViewController: UIViewController {
 
 extension ViewController:UITableViewDelegate,UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return 30
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
